12639a9f983273cb170575bf1bf4770343cffbe6
Arshad Sheikh <7144519998usa@gmail.com>
2026-06-05 14:31:17 -0700

    Bypass middleware for sitemap routes

--- middleware.js
+++ middleware.js
@@ -97,5 +97,5 @@ export function middleware(request) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml).*)"]
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml|sitemap_index.xml|sitemaps/).*)"]
 };
